7b10b6fd99cf0743ad5410b08b5dfbd768e64a11
Ahmad Bani Yaseen <ahmadbaniyassen8@gmail.com>
2024-10-24 22:05:58 +0300

    Build all fils and some fixes serch filter

--- BusinessCardUI/src/app/app.component.ts
+++ BusinessCardUI/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { BusinessCardComponent } from './business-card.component'; 
+import { CreateBusinssCardComponent } from './create-businss-card/create-businss-card.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BusinessCardComponent, CreateBusinssCardComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- BusinessCardUI/src/app/app.routes.ts
+++ BusinessCardUI/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { BusinessCardComponent } from './business-card.component';
+import { CreateBusinssCardComponent } from './create-businss-card/create-businss-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'GetAllCards',
+        pathMatch: 'full'
+    },
+    {
+        path: 'Create',
+        component: CreateBusinssCardComponent
+    },
+    {
+        path: 'GetAllCards',
+        component: BusinessCardComponent
+    } 
+];
+
